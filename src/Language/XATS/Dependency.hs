cdcf21ab6532ce511b079d1ad6e434e4f400c3a8
Vanessa McHale <vanessa.mchale@iohk.io>
2018-10-31 21:53:03 -0500

    add getAll function

--- src/Language/XATS/Dependency.hs
+++ src/Language/XATS/Dependency.hs
@@ -2,10 +2,13 @@
 
 -- | Dependency analysis for ATS.
 module Language.XATS.Dependency ( getDeps
+                                , getAll
                                 ) where
 
 import           Control.Monad            (filterM)
 import qualified Data.ByteString.Lazy     as BSL
+import           Data.Foldable            (fold)
+import           Data.List                (group, sort)
 import qualified Data.Text                as T
 import           Language.XATS.Lexer
 import           Language.XATS.Type.Lexer
@@ -37,3 +40,15 @@ extractDeps = fmap extractTokDeps . lexXATS
           extractTokDeps (TokKeyword _ Dynload : TokString _ str : toks) = str : extractTokDeps toks
           extractTokDeps (TokKeyword _ Symload : TokString _ str : toks) = str : extractTokDeps toks
           extractTokDeps (_ : toks)                                      = extractTokDeps toks
+
+-- | Get dependencies recursively.
+getAll :: FilePath
+       -> IO [FilePath]
+getAll src = do
+    deps <- getDeps src
+    level <- traverse getAll deps
+    let rmdups = fmap head . group . sort
+        next = rmdups (fold (deps : level))
+    pure $ if null level
+        then deps
+        else next
